59c3a6611f8bbbce4b2306c8e3d4fb3a938a3ca2
Fernando <andresfernandoxd1591@gmail.com>
2023-09-13 09:10:53 -0500

    db config changed

--- src/db.js
+++ src/db.js
@@ -3,5 +3,7 @@ import { createPool } from "mysql2/promise";
 export const pool = createPool({
     host:'localhost',
     user:'root',
+    password:'admi',
+    port:3306,
     database:'companydb'
 })
